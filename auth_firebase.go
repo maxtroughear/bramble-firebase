855b273f2f5c806ad2e0107945f23d2696c6a0a1
Max Troughear <max@maxtroughear.dev>
2022-03-05 01:28:24 +1300

    Initial firebase auth implementation

---
+++ auth_firebase.go
@@ -0,0 +1,173 @@
+package bramblefirebase
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+
+	firebase "firebase.google.com/go/v4"
+	"firebase.google.com/go/v4/auth"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/golang-jwt/jwt/v4/request"
+	"github.com/movio/bramble"
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/api/option"
+)
+
+func init() {
+	bramble.RegisterPlugin(NewFirebasePlugin("", nil))
+}
+
+func NewFirebasePlugin(credentialsFilePath string, roles map[string]bramble.OperationPermissions) *FirebasePlugin {
+	return &FirebasePlugin{
+		config: FirebasePluginConfig{
+			ServiceAccountFilePath: credentialsFilePath,
+			Roles:                  roles,
+		},
+		jwtExtractorr: request.MultiExtractor{
+			request.AuthorizationHeaderExtractor,
+			cookieTokenExtractor{cookieName: "token"},
+		},
+	}
+}
+
+type FirebasePlugin struct {
+	app           *firebase.App
+	auth          *auth.Client
+	config        FirebasePluginConfig
+	jwtExtractorr request.Extractor
+
+	bramble.BasePlugin
+}
+
+type FirebasePluginConfig struct {
+	ServiceAccountFilePath string                                  `json:"service-account-file"`
+	RefreshTokenFilePath   string                                  `json:"refresh-token-file"`
+	ProjectID              string                                  `json:"project-id"`
+	Roles                  map[string]bramble.OperationPermissions `json:"roles"`
+}
+
+func (p *FirebasePlugin) ID() string {
+	return "auth-firebase"
+}
+
+func (p *FirebasePlugin) Configure(cfg *bramble.Config, data json.RawMessage) error {
+	err := json.Unmarshal(data, &p.config)
+	if err != nil {
+		return err
+	}
+
+	if len(p.config.ServiceAccountFilePath) > 0 {
+		opt := option.WithCredentialsFile(p.config.ServiceAccountFilePath)
+		firebaseApp, err := firebase.NewApp(context.Background(), nil, opt)
+		if err != nil {
+			return err
+		}
+		p.app = firebaseApp
+	} else if len(p.config.RefreshTokenFilePath) > 0 && len(p.config.ProjectID) > 0 {
+		opt := option.WithCredentialsFile(p.config.RefreshTokenFilePath)
+		firebaseConfig := &firebase.Config{
+			ProjectID: p.config.ProjectID,
+		}
+		firebaseApp, err := firebase.NewApp(context.Background(), firebaseConfig, opt)
+		if err != nil {
+			return err
+		}
+		p.app = firebaseApp
+	} else {
+		firebaseApp, err := firebase.NewApp(context.Background(), nil, nil)
+		if err != nil {
+			return err
+		}
+		p.app = firebaseApp
+	}
+
+	auth, err := p.app.Auth(context.Background())
+	if err != nil {
+		return err
+	}
+	p.auth = auth
+
+	return nil
+}
+
+type Claims struct {
+	jwt.StandardClaims
+	Role string
+}
+
+func (p *FirebasePlugin) ApplyMiddlewarePublicMux(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		tokenStr, err := p.jwtExtractorr.ExtractToken(r)
+		if err != nil {
+			// unauthenticated request, must use "public_role"
+			log.Info("unauthenticated request")
+			r = r.WithContext(bramble.AddPermissionsToContext(r.Context(), p.config.Roles["public_role"]))
+			h.ServeHTTP(rw, r)
+			return
+		}
+
+		token, err := p.auth.VerifyIDToken(r.Context(), tokenStr)
+		if err != nil {
+			log.WithError(err).Info("invalid token")
+			rw.WriteHeader(http.StatusUnauthorized)
+			writeGraphqlError(rw, "invalid token")
+			return
+		}
+
+		tokenRole := token.Claims["role"].(string)
+
+		role, ok := p.config.Roles[tokenRole]
+		if !ok {
+			log.WithField("role", tokenRole).Info("invalid role")
+			rw.WriteHeader(http.StatusUnauthorized)
+			writeGraphqlError(rw, "invalid role")
+			return
+		}
+
+		bramble.AddFields(r.Context(), bramble.EventFields{
+			"role":    tokenRole,
+			"subject": token.Subject,
+		})
+
+		ctx := r.Context()
+		ctx = bramble.AddPermissionsToContext(ctx, role)
+		ctx = addStandardJWTClaimsToOutgoingRequest(ctx, token)
+		ctx = bramble.AddOutgoingRequestsHeaderToContext(ctx, "JWT-Claim-Role", tokenRole)
+		h.ServeHTTP(rw, r.WithContext(ctx))
+	})
+}
+
+func addStandardJWTClaimsToOutgoingRequest(ctx context.Context, token *auth.Token) context.Context {
+	if token.Audience != "" {
+		ctx = bramble.AddOutgoingRequestsHeaderToContext(ctx, "JWT-Claim-Audience", token.Audience)
+	}
+	if token.UID != "" {
+		ctx = bramble.AddOutgoingRequestsHeaderToContext(ctx, "JWT-Claim-UID", token.UID)
+	}
+	if token.Issuer != "" {
+		ctx = bramble.AddOutgoingRequestsHeaderToContext(ctx, "JWT-Claim-Issuer", token.Issuer)
+	}
+	if token.Subject != "" {
+		ctx = bramble.AddOutgoingRequestsHeaderToContext(ctx, "JWT-Claim-Subject", token.Subject)
+	}
+	return ctx
+}
+
+func writeGraphqlError(w io.Writer, message string) {
+	json.NewEncoder(w).Encode(bramble.Response{Errors: bramble.GraphqlErrors{{Message: message}}})
+}
+
+// cookieTokenExtractor extracts a JWT token from the "token" cookie
+type cookieTokenExtractor struct {
+	cookieName string
+}
+
+func (c cookieTokenExtractor) ExtractToken(r *http.Request) (string, error) {
+	cookie, err := r.Cookie(c.cookieName)
+	if err != nil {
+		return "", request.ErrNoTokenInRequest
+	}
+	return cookie.Value, nil
+}
---
+++ go.mod
@@ -0,0 +1,46 @@
+module github.com/maxtroughear/bramblefirebase
+
+go 1.17
+
+require (
+	firebase.google.com/go/v4 v4.7.1
+	github.com/golang-jwt/jwt/v4 v4.3.0
+	github.com/movio/bramble v1.3.4
+	github.com/sirupsen/logrus v1.8.1
+	google.golang.org/api v0.57.0
+)
+
+require (
+	cloud.google.com/go v0.94.1 // indirect
+	cloud.google.com/go/firestore v1.5.0 // indirect
+	cloud.google.com/go/storage v1.10.0 // indirect
+	github.com/99designs/gqlgen v0.14.0 // indirect
+	github.com/agnivade/levenshtein v1.1.1 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/felixge/httpsnoop v1.0.2 // indirect
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/googleapis/gax-go/v2 v2.1.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/mitchellh/mapstructure v1.4.2 // indirect
+	github.com/prometheus/client_golang v1.11.0 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.31.1 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
+	github.com/vektah/gqlparser/v2 v2.2.0 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/net v0.0.0-20210525063256-abc453219eb5 // indirect
+	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20211003122950-b1ebd4e1001c // indirect
+	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20210903162649-d08c68adba83 // indirect
+	google.golang.org/grpc v1.40.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
